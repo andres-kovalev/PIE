bbd41bb86face466f8c87b2e7f44b19ddbd7cefc
Andres <root@cha0s.ru>
2012-11-13 18:10:20 +0400

    PIE 1.0 (beta)

---
+++ asserts/styles.css
@@ -0,0 +1,179 @@
+body {
+	padding-top: 60px;
+}
+
+div.home {
+	margin-top: -20px;
+	padding: 30px 0;
+	text-align: center;
+	background-color: #f5f5f5;
+	border-bottom: 1px solid #e5e5e5;
+}
+
+div.reasons {
+	margin-top: 20px;
+	margin-bottom: 20px;
+	width: 900px;
+	text-align: center;
+}
+
+div.reasons .well {
+	padding: 10px;
+	height: 200px;
+	background-color: #fafafa;
+}
+
+div.reasons h1 {
+	margin-bottom: 20px;
+}
+
+div.reasons .well, div.reasons .span3, div.reasons .span3 h3 {
+	width: 255px;
+}
+
+div.home h2, div.home p, div.home a {
+	margin: 15px 0;
+}
+
+div.home h1 {
+	margin: 50px 0;
+	font-size: 80px;
+	font-weight: bold;
+}
+
+.masthead-links {
+	margin: 0;
+	list-style: none;
+}
+
+.masthead-links li {
+	display: inline;
+	padding: 0 10px;
+}
+
+section {
+	padding-top: 40px;
+	padding-left: 15px;
+}
+
+.spy-content {
+	margin-top: -35px;
+}
+
+.debug {
+	background-color: #f00;
+}
+
+#scrollspy-menu {
+	width: 198px;
+}
+
+.affix-container .affix {
+	top: 60px;
+}
+
+.affix-bottom {
+	position: fixed;
+	bottom: 200px;
+	width: 500px;
+}
+
+table.table code.prettyprint {
+	border: 0;
+	background-color: transparent;
+}
+
+table.table th.col1 {
+	width: 230px;
+}
+
+pre.prettyprint {
+	margin-bottom: 20px;
+}
+
+.prettyprint {
+	padding: 8px;
+	background-color: #f7f7f9;
+	border: 1px solid #e1e1e8;
+}
+
+.prettyprint.linenums {
+	-webkit-box-shadow: inset 40px 0 0 #fbfbfc, inset 41px 0 0 #ececf0;
+	-moz-box-shadow: inset 40px 0 0 #fbfbfc, inset 41px 0 0 #ececf0;
+	box-shadow: inset 40px 0 0 #fbfbfc, inset 41px 0 0 #ececf0;
+}
+
+ol.linenums {
+	margin: 0 0 0 33px;
+}
+
+ol.linenums li {
+	padding-left: 12px;
+	color: #bebec5;
+	line-height: 20px;
+	text-shadow: 0 1px 0 #fff;
+}
+
+.pln {
+	color: #000;
+}
+
+.str {
+	color: #080;
+}
+
+.kwd {
+	color: #008;
+}
+
+.pun {
+	color: #660;
+}
+
+.lit{
+	color: #066;
+}
+
+.com{
+	color: #800;
+}
+
+code a, code a:hover {
+	color: #d14;
+	text-decoration: none;
+}
+
+.alert blockquote {
+	margin: 10px 0;
+}
+
+.alert blockquote pre {
+	margin: 0;
+	border: 0;
+	background-color: transparent;
+}
+
+.footer {
+	padding: 50px 0;
+	margin-top: 10px;
+	border-top: 1px solid #e5e5e5;
+	background-color: #f5f5f5;
+}
+
+.footer p {
+	margin-bottom: 0;
+	color: #777;
+}
+
+.footer-links {
+	margin: 10px 0;
+}
+
+.footer-links li {
+	display: inline;
+	padding: 0 2px;
+}
+
+.footer-links li:first-child {
+	padding-left: 0;
+}
